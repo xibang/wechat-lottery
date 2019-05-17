f844e4d0e40d37cd8613c0a362e1e1022ccd3640
willin <willin@willin.org>
2019-05-17 11:49:06 +0800

    init

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const { callback: wechatCallbackHandler, login: wechatLoginHandler } = require('./wechat');
-const { check: userCheckHandler } = require('./user');
+const { check: userCheckHandler, feeling: userFeelingHandler } = require('./user');
 
 module.exports = (fastify, opts, next) => {
   fastify.get('/wechat/login', wechatLoginHandler);
   fastify.get('/wechat/callback', wechatCallbackHandler);
   fastify.get('/user/check', userCheckHandler);
+  fastify.get('/user/feeling', userFeelingHandler);
+
   next();
 };
